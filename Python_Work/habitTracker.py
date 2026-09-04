a9e1725b670b6d8d34880a779e4432b34d95fa14
PossibilityJoshuaB <joshmbyju@gmail.com>
2026-09-04 16:14:20 -0400

    Updated Habit Tracker to use sqlite database

--- Python_Work/habitTracker.py
+++ Python_Work/habitTracker.py
@@ -1,6 +1,18 @@
 habits = []
 logs = []
+
+import sqlite3
+dbConnection = sqlite3.connect("tutorial.db")
+cursor = dbConnection.cursor()
+
+cursor.execute("CREATE TABLE IF NOT EXISTS habits (" \
+"id INTEGER PRIMARY_KEY," \
+"habit_name VARCHAR(16)," \
+"habit_progress INTEGER," \
+"habit_target INTEGER)")
+
 print("Welcome to Habit Tracker. What would you like to do today?")
+print()
 
 while True:
     print(
@@ -9,39 +21,91 @@ while True:
         "Enter 'l' to enter a log\n" \
         "Enter 'v' to view habits\n" 
         "Enter 'quit' to exit: ")
+
     userChoice = input("Enter: ")
     print()
+    if cursor.fetchone() != None:
+        lastID = cursor.execute("SELECT id FROM habits ORDER BY id DESC").fetchone()[0]
+    else:
+        lastID = 0
+
 
     if userChoice == "h":
         print("What habit would you like to start?")
         userHabitName = input("Enter: ")
         print()
+
         print("How long would you like to track this habit? (enter number in days)")
         userHabitDuration = int(input("Enter: "))
+        print()
+
         logs.append([userHabitName, 0, userHabitDuration])
-        print("Habit logged\n")
+
+        cursor.execute("INSERT INTO habits (habit_name, habit_progress, habit_target) " \
+        "VALUES (?, ?, ?)",
+        (userHabitName, 0, userHabitDuration))
+        dbConnection.commit()
+
+        print("Habit started")
+        print()
     elif userChoice == "l":
         print("What habit would you like to log?")
         habitToLog = input("Enter: ")
-        for x in range(len(logs)):
-            logEntry = logs[x]
-            if habitToLog == logEntry[0]:
-                logEntry[1] += 1
-                print(f"Logged: {logEntry[1]} out of targeted {logEntry[2]} days completed")
-                if logEntry[2] == logEntry[1]:
-                    print(f"You have completed your target of {logEntry[0]} for {logEntry[2]} days.")
-                    print(f"Would you like to continue {logEntry[0]}")
-                    userAns = input("y/n: ")
-                    if userAns == "y":
-                        print("How long would you like to track this habit? (enter number in days)")
-                        logEntry[2] += int(input("Enter: "))
-                    else:
-                        logs.remove(logEntry)
-    elif userChoice == "quit":
-        print("Have a good day!")
-        break
-    elif userChoice == "v":
-        for entry in logs:
-            print(f"Habit: {entry[0]}\nCompleted {entry[1]} out of targeted {entry[2]} days")
+        print()
+        print("How many days have you completed of this habit since your last update?")
+        daysToLog = int(input("Enter number of days: "))
+        print()
+
+        currentCompletion = cursor.execute(f"SELECT habit_progress FROM habits WHERE habit_name = ?", (habitToLog,)).fetchone()[0]
+        target = cursor.execute(f"SELECT habit_target FROM habits WHERE habit_name = ?", (habitToLog,)).fetchone()[0]
+
+        cursor.execute(f"UPDATE habits SET habit_progress = {currentCompletion + daysToLog} WHERE habit_name = ?", (habitToLog,))
+        dbConnection.commit()
+        print("Habit logged")
+        print()
+
+        daysLeftToTarget = target - currentCompletion
+        if daysLeftToTarget == 0:
+            print(f"You have completed your target of {target} days.")
+            print("Would you like to continue this habit or remove it from the tracker and move on?")
+            ans = input("Enter y/n: ")
+            print()
+            if ans == "y":
+                print("How long would you like to extend your target for?")
+                targetExtension = int(input("Enter number of days: "))
+                print()
+
+                cursor.execute(f"UPDATE habits SET habit_target = {target + targetExtension} WHERE habit_name = ?", (habitToLog,))
+                dbConnection.commit()
+            else:
+                print("Congratulations on completing your goal")
+                print()
+
+                cursor.execute(f"DELETE FROM habits WHERE habit_name = ?", (habitToLog,))
+        else:
+            print(f"Logged: You have completed {currentCompletion} days out of your targeted {target} days.")
             print()
 
+    elif userChoice == "quit":
+
+        print("Have a good day!")
+        print()
+        break
+
+    elif userChoice == "v":
+        allHabits = cursor.execute("SELECT * FROM habits").fetchall()
+        for habit in allHabits:
+            print(f"Habit: {habit[1]}\nCompleted {habit[2]} out of targeted {habit[3]} days")
+            print()
+
+    elif userChoice == "delete":
+        userAunthetication = input("Enter passcode for admin privileges: ")
+        if userAunthetication == "MEGATR0N":
+            cursor.execute("DELETE FROM habits")
+            print("Table cleared")
+            print()
+        else:
+            break
+    else:
+        print("Please enter a valid input")
+        print()
